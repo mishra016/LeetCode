fdbf37900c0749e3947be6a8bbf9e954922576fd
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2023-10-15 18:04:59 -0400

    Time: 1 ms (74.4%), Space: 40.8 MB (13.36%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,12 +1,20 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int low=0 , high = citations.length;
-        while(low < high){
-            int mid = (low+high+1)/2;
-            int cnt=0;
-            for(int i=0 ; i<citations.length ; i++) if(citations[i] >= mid) cnt++;
-            if(cnt >= mid) low = mid;
-            else high = high=mid-1;
+        int high = citations.length, low = 0;
+        while(low<high){
+            int mid= (low+high+1)/2;
+            int count=0;
+            for(int i=0; i<citations.length; i++){
+                if(citations[i] >= mid){
+                    count++;
+                }
+            }
+            if(count >=mid){
+                low = mid;
+            }
+            else{
+                high = mid-1;
+            }
         }
         return low;
     }
