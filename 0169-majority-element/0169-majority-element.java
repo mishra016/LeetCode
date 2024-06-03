a4d3a4dca1afdbc69c14363d977b05c5a19eace9
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-06-03 14:12:34 -0400

    Time: 1 ms (99.81%), Space: 53.7 MB (6.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,53 @@
+//Follow-Up using Boyer-Moore Voting Algorithm
 class Solution {
     public int majorityElement(int[] nums) {
 
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int candidate = 0;
+        int votes = 0;
+
+        for(int num: nums){
+            if(votes == 0){
+                candidate = num;
+            }
+            if(num == candidate){
+                votes++;
+            }else{
+                votes--;
+            }
+        }
+
+        return candidate;
         
     }
 }
+
+// ----------------------------------------------------------------------
+
+// //HashMap
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for(int num: nums){
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+
+//             if(map.get(num) > (nums.length/2))
+//             return num;
+//         }
+
+//         return 0; // will never reach
+        
+//     }
+// }
+
+
+// ----------------------------------------------------------------------
+
+// //Sorting
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Arrays.sort(nums);
+//         return nums[nums.length/2];
+        
+//     }
+// }
