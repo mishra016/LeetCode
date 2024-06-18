c50e05365ec4a57e581d405ed8bd3fbffdad99d1
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-06-17 21:31:25 -0400

    Time: 0 ms (100%), Space: 41.5 MB (72.4%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,39 +1,74 @@
+
 class Solution {
     public void sortColors(int[] nums) {
-        //merge/quick sort gives --> TC:O(nlogn)
+        int n= nums.length;
 
-        //traverse and count --> Tc: O(n)
-        int zeros=0, ones=0, twos = 0;
+        //one-pass --> Tc: O(n)
+        int i=0;
+        int j=0; 
+        int k = n-1;
 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==0){
-                zeros++;
+        while(j<=k){
+            if(nums[j] == 1){
+                j++;
             }
-            else if(nums[i]==1){
-                ones++;
+            else if(nums[j] == 2){
+                int temp = nums[k];
+                nums[k] = nums[j];
+                nums[j] = temp;
+                k--;
             }
-            else{
-                twos++;
+            else{ //nums[j] == 0
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j++;
+
             }
         }
-
-        for(int i=0; i<nums.length; i++){
-            if(zeros > 0){
-                nums[i] = 0;
-                zeros--;
-            }else if(ones > 0){
-                nums[i] = 1;
-                ones--;
-            }else if(twos > 0){
-                nums[i] = 2;
-                twos--;
-            }
-        } 
-
         
         
     }
 }
+
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         //merge/quick sort gives --> TC:O(nlogn)
+
+//         //traverse and count --> Tc: O(n)
+//         int zeros=0, ones=0, twos = 0;
+
+//         for(int i=0; i<nums.length; i++){
+//             if(nums[i]==0){
+//                 zeros++;
+//             }
+//             else if(nums[i]==1){
+//                 ones++;
+//             }
+//             else{
+//                 twos++;
+//             }
+//         }
+
+//         for(int i=0; i<nums.length; i++){
+//             if(zeros > 0){
+//                 nums[i] = 0;
+//                 zeros--;
+//             }else if(ones > 0){
+//                 nums[i] = 1;
+//                 ones--;
+//             }else if(twos > 0){
+//                 nums[i] = 2;
+//                 twos--;
+//             }
+//         } 
+
+        
+        
+//     }
+// }
 
 
 
