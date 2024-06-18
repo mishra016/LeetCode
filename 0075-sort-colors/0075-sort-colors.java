d671aceb0223742e1cdfe348c89c04f40fa8895c
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-06-17 21:15:00 -0400

    Time: 0 ms (100%), Space: 41.4 MB (92.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,17 +1,57 @@
 class Solution {
     public void sortColors(int[] nums) {
+        //merge/quick sort gives --> TC:O(nlogn)
 
-        for(int i=0; i<nums.length-1; i++){
-            for(int j=0; j<nums.length-1-i; j++){
-                if(nums[j] > nums[j+1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
+        //traverse and count --> Tc: O(n)
+        int zeros=0, ones=0, twos = 0;
 
-                }
-
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                zeros++;
+            }
+            else if(nums[i]==1){
+                ones++;
+            }
+            else{
+                twos++;
             }
         }
+
+        for(int i=0; i<nums.length; i++){
+            if(zeros > 0){
+                nums[i] = 0;
+                zeros--;
+            }else if(ones > 0){
+                nums[i] = 1;
+                ones--;
+            }else if(twos > 0){
+                nums[i] = 2;
+                twos--;
+            }
+        } 
+
+        
         
     }
 }
+
+
+
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         //bubble sort --> TC: o(n^2)
+//         for(int i=0; i<nums.length-1; i++){
+//             for(int j=0; j<nums.length-1-i; j++){
+//                 if(nums[j] > nums[j+1]){
+//                     int temp = nums[j];
+//                     nums[j] = nums[j+1];
+//                     nums[j+1] = temp;
+
+//                 }
+
+//             }
+//         }
+        
+//     }
+// }
