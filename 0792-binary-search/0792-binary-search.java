18a78d904ba8b54ae80dcdea758a75b1b5fa8c71
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-06-20 21:53:07 -0400

    Time: 0 ms (100%), Space: 45 MB (98.23%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,13 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == target){
-                return i;
+        //TC: O(logn)
+
+        int start =0;
+        int end = nums.length - 1;
+
+        while(start<= end){
+            int mid = (start + end)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
             }
         }
-
-        return -1;
         
+        return -1;
     }
 }
+
+
+// class Solution {
+//     public int search(int[] nums, int target) {
+
+//         //TC: O(N)
+//         for(int i=0; i<nums.length; i++){
+//             if(nums[i] == target){
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+        
+//     }
+// }
