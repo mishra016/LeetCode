974cc46da51f666423e5ed66447a00be658f4940
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-07-31 19:58:04 -0400

    Time: 3 ms (43.41%), Space: 45 MB (10.46%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.java
+++ 3226-minimum-number-game/3226-minimum-number-game.java
@@ -3,18 +3,35 @@ class Solution {
 
         Arrays.sort(nums);
 
-        int[] arr = new int[nums.length];
+        for(int i=0; i<nums.length-1; i+=2){
+            int temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
 
-        for(int i=0; i<nums.length; i++){
-            if(i%2 != 0){
-                arr[i-1] = nums[i];
-            }
-            else{
-                arr[i+1] = nums[i];
-            }
         }
 
-        return arr;
+        return nums;
         
     }
 }
+
+// class Solution {
+//     public int[] numberGame(int[] nums) {
+
+//         Arrays.sort(nums);
+
+//         int[] arr = new int[nums.length];
+
+//         for(int i=0; i<nums.length; i++){
+//             if(i%2 != 0){
+//                 arr[i-1] = nums[i];
+//             }
+//             else{
+//                 arr[i+1] = nums[i];
+//             }
+//         }
+
+//         return arr;
+        
+//     }
+// }
