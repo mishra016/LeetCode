712c9bff541db4afa578770aac20f2b6f39ed986
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-07-31 20:18:31 -0400

    Time: 3 ms (43.41%), Space: 45 MB (10.46%) - LeetHub

--- 3226-minimum-number-game/3226-minimum-number-game.java
+++ 3226-minimum-number-game/3226-minimum-number-game.java
@@ -1,19 +1,51 @@
 class Solution {
     public int[] numberGame(int[] nums) {
 
-        Arrays.sort(nums);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int[] arr = new int[nums.length];
 
-        for(int i=0; i<nums.length-1; i+=2){
-            int temp = nums[i];
-            nums[i] = nums[i+1];
-            nums[i+1] = temp;
-
+        for(int num:nums){
+            minHeap.add(num);
         }
 
-        return nums;
+        int index = 0;
+
+        while(!minHeap.isEmpty()){
+
+            int alice = minHeap.poll();
+            int bob = minHeap.poll();
+
+            arr[index++] = bob;
+            arr[index++] = alice;
+        }
+
+        return arr;
         
     }
 }
+
+
+// -------------------------------------------------------
+
+// class Solution {
+//     public int[] numberGame(int[] nums) {
+
+//         Arrays.sort(nums);
+
+//         for(int i=0; i<nums.length-1; i+=2){
+//             int temp = nums[i];
+//             nums[i] = nums[i+1];
+//             nums[i+1] = temp;
+
+//         }
+
+//         return nums;
+        
+//     }
+// }
+
+
+//------------------------------------------------------
 
 // class Solution {
 //     public int[] numberGame(int[] nums) {
