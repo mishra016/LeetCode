8292fc5175ac74abe1ca3d120dca8b8bded8b40c
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-08-12 14:18:22 -0400

    Time: 13 ms (31.47%), Space: 44.9 MB (7.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        if(s.length() != t.length()){
-            return false;
-        }
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
 
         HashMap<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<s.length();i++){
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
+        // for(int i=0; i<s.length();i++){
+        //     map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        //     map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
+        // }
+        for(char ch: s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        // for(char ch: s.toCharArray()){
-        //     map.put(ch,map.getOrDefault(ch,0)+1);
-        // }
 
-        // for(char ch: t.toCharArray()){
-        //     map.put(ch,map.getOrDefault(ch,0) - 1);
-        // }
+        for(char ch: t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0) - 1);
+        }
 
         for(int val: map.values()){
             if(val!=0){
