745f8f303ceaf2cd738d1ffac2bc0b05cd3bd49c
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-08-12 12:32:47 -0400

    Time: 20 ms (19.68%), Space: 55.2 MB (83.81%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,31 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        Arrays.sort(nums);
 
-        for(int i=0; i<nums.length; i++){
-            if(set.contains(nums[i])){
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
                 return true;
             }
-
-            set.add(nums[i]);
         }
 
-        return false; //TC: O(n)
+        return false;
     }
 }
-
-// --------------------------------------------------------------
-
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//         Arrays.sort(nums);
-
-//         for(int i=0; i<nums.length-1; i++){
-//             if(nums[i] == nums[i+1]){
-//                 return true;
-//             }
-//         }
-        
-//         return false; //TC: O(nlogn)
-//     }
-// }
