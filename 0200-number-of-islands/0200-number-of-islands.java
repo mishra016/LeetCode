531076c34fb00487d342f34d134fb78952d40a9a
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-11-09 16:13:03 -0800

    Time: 3 ms (87.29%), Space: 49.8 MB (34.07%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,36 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
+    public void dfs(char[][] grid, int row, int col){
+        //check boundary
+        if(row<0 || col<0 || row >= grid.length || col >= grid[0].length|| grid[row][col] == '0'){
+            return;
         }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
+
+        //mark cell as visited
+        grid[row][col] = '0';
+
+        //visit adj cells
+        dfs(grid, row+1, col);
+        dfs(grid, row-1, col);
+        dfs(grid, row, col+1);
+        dfs(grid, row, col-1);
+    }
+
+    public int numIslands(char[][] grid) {
+
+        int numberIsland = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j< grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    numberIsland++;
                     dfs(grid, i, j);
                 }
             }
         }
+
+        return numberIsland;
         
-        return numIslands;
-    }
-    
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
-            return;
-        }
-        
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
     }
 }
