8a6e4e4e9bb5900db2b48227cee556ebdd7b986e
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-11-12 07:59:44 -0800

    Time: 1 ms (11.58%), Space: 41.4 MB (86.62%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,29 +1,10 @@
 public class Solution {
     public int reverseBits(int n) {
-        StringBuilder binary = new StringBuilder();
-        
-        // Convert `n` to binary string representation by checking each bit using modulo
-        for (int i = 0; i < 32; i++) {
-            // Check if the least significant bit is 1
-            if ((n & (1 << i)) != 0) {
-                binary.append("1");
-            } else {
-                binary.append("0");
-            }
-
-        }
-        
         int res = 0;
-        String reversedBinary = binary.reverse().toString();
-        
-        // Reconstruct the reversed integer using powers of 2 (instead of bitwise OR)
         for (int i = 0; i < 32; i++) {
-            if (reversedBinary.charAt(i) == '1') {
-                // Using Math.pow(2, i) to add the value at the i-th position
-                res |= (1 << i);
-            }
+            int bit = (n >> i) & 1;
+            res += (bit << (31 - i));
         }
-        
         return res;
     }
 }
