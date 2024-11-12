06c66cf829e394ad064f2202c607acd74c73c3e3
Mohi Mishra <62807322+mishra016@users.noreply.github.com>
2024-11-12 08:13:45 -0800

    Time: 1 ms (100%), Space: 45.8 MB (32.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,16 +1,24 @@
+
 class Solution {
+//Greedy Approach: The idea behind this approach is that we try to "jump backwards" from the end of the array to the start. If we can reach the current "goal" from any position, we update the goal to that position and continue. If we can eventually set the goal to 0, it means we can reach the end. TC: O(N)
     public boolean canJump(int[] nums) {
-        int goal = nums.length-1;
-        for(int i=nums.length-1; i>=0; i--){
-            if(i +nums[i] >=goal){
+        // Set the goal as the last index of the array
+        int goal = nums.length - 1;
+
+        // Traverse the array from the last index towards the first
+        for (int i = nums.length - 1; i >= 0; i--) {
+            // Check if we can jump from index `i` to the goal or beyond
+            if (i + nums[i] >= goal) {
+                // If we can, update the goal to the current index `i`
                 goal = i;
             }
         }
-        if(goal == 0){
+
+        // If we end up at the first index (`goal == 0`), we can reach the end
+        if (goal == 0) {
             return true;
-        }else{
+        } else {
             return false;
         }
-        
     }
 }
